20857c94561ebd19e68140e446665bf836d379b9
Brian Law <gmbrianlaw@gmail.com>
2022-03-22 14:54:05 -0700

    Improve webiste

---
+++ assets/javascript/index.js
@@ -0,0 +1,9 @@
+const messageContainer = document.getElementById("message-container");
+
+function hideMessage() {
+    messageContainer.style.display = "none";
+}
+
+function showMessage() {
+    messageContainer.style.display = "block";
+}
